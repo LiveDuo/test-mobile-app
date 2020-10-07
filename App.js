18da58b429e19fcf7280e04c3c6ad3bce360fa3f
Andreas Tzionis <Andy0n7@gmail.com>
2020-10-07 03:33:45 +0300

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.paragraph}>
-          This is dynamic!
+          This is dynamic 3!
         </Text>
       </View>
     );
