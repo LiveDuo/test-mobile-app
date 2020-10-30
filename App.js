2449fcfe7f0e49a15ed0ed64b8f5e4ef23d0e4cf
Andreas Tzionis <Andy0n7@gmail.com>
2020-10-30 11:15:33 +0200

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.paragraph}>
-          Hello Nikoli newer 23sdeasdasddeadasasdasdasd
+          Hello Nikoli newer 23sdeasdasddeadasasdasdasdas
         </Text>
         <Text style={styles.paragraph}>
           New element
